87a2daa4bef6d42c6b58b59c0ae9e9bcb0557364
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-09-08 02:18:54 +0300

    conditional statements

---
+++ if-else-if/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            Console.WriteLine(time);
+            if(time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın.");
+            }
+            else if(time >= 11 && time < 18)
+            {
+                Console.WriteLine("İyi Günler.");
+            }
+            else if (time >= 18 && time <= 23)
+            {
+                Console.WriteLine("İyi Akşamlar.");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+
+            //ternary
+            string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";
+
+            sonuc = time >= 6 && time <= 11 ? "Günaydın" : time >= 11 && time < 18 ? "İyi akşamlar" : "İyi geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
