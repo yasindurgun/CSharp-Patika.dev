73ec530feabcca5a44f5d98440cc114cdb568527
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-09-08 17:15:05 +0300

    loops

---
+++ Donguler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tekToplam += i;
+                }
+                else
+                    ciftToplam += i;
+            }
+            Console.WriteLine($"Tek Toplam: {tekToplam}");
+            Console.WriteLine($"Çift Toplam: {ciftToplam}");
+
+            //break, continue
+            for (int i = 1; i < 10; i++)
+            {
+                if (i==4)
+                {
+                    //break;
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace donguler_while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+            
+
+            char character = 'a';
+            while (character <= 'z')
+            {
+                Console.Write(character + " ");
+                character++;
+            }
+
+            Console.WriteLine("***** Foreach *****");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach (var item in arabalar)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
