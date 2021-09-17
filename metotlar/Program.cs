c09913b12f32cd164a73a050045d84a9db34fd02
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-09-17 16:48:53 +0300

    methods

---
+++ metotlar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 7;
+            int b = 3;
+
+            Console.WriteLine(a+b);
+            int sonuc = Topla(a,b);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+
+            Console.ReadKey();
+        }
+
+        static int Topla(int deger1, int deger2)
+        {
+            return deger1 + deger2;
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdir(String veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1 += 1;
+            deger2 += 1;
+
+            return deger1 + deger2;
+        }
+    }
+}
---
+++ recursive_extension_metotlar/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace recursive_extension_metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rekürsif - Öz Yinelemeli
+            //3^4
+
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result = result*3;
+            }
+            Console.WriteLine(result);
+            Islemler instance = new();
+            Console.WriteLine(instance.Expo(3,4));
+
+            //Extension Metotlar
+            string ifade = "Yasin Durgun";
+            bool sonuc = ifade.CheckSpaces();
+            Console.WriteLine(sonuc);
+
+            if (sonuc)
+            {
+                Console.WriteLine(ifade.RemoveWhiteSpaces());
+                Console.WriteLine(ifade.MakeUpperCase());
+            }
+
+            int[] dizi = { 9, 3, 6, 2, 1, 5, 0 };
+            dizi.SortArray();
+            dizi.EkranaYazdir();
+
+            int sayi = 5;
+            Console.WriteLine(sayi.IsEven());
+            Console.WriteLine(ifade.getFirstCharacter());
+            Console.ReadKey();
+        }
+    }
+
+    public class Islemler
+    {
+        public int Expo(int sayi, int üs)
+        {
+            if (üs < 2)
+            {
+                return sayi;
+            }
+            return Expo(sayi, üs - 1) * sayi;
+        }
+        //Expo(3,4)
+        //Expo(3,3) * 3
+        //Expo(3,2) * 3 * 3
+        //Expo(3,1) * 3 * 3 * 3
+        //3*3*3*3 = 3^4
+    }
+
+    public static class Extension
+    {
+        public static bool CheckSpaces(this string param)
+        {
+            return param.Contains(" ");
+        }
+
+        public static string RemoveWhiteSpaces(this string param)
+        {
+            string[] dizi = param.Split(" ");
+            return string.Join("*", dizi);
+        }
+
+        public static string MakeUpperCase(this string param)
+        {
+            return param.ToUpper();
+        }
+
+        public static int[] SortArray(this int[] param)
+        {
+            Array.Sort(param);
+            return param;
+        }
+
+        public static void EkranaYazdir(this int[] param)
+        {
+            foreach (var item in param)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static bool IsEven(this int param)
+        {
+            return param % 2 == 0;
+        }
+
+        public static string getFirstCharacter(this string param)
+        {
+            return param.Substring(0,1);
+        }
+    }
+
+}
