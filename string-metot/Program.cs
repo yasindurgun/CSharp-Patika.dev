f15a448f9ccdaf5706e9538d7d90ee2db58baba9
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-10-08 19:32:19 +0300

    string operations

---
+++ string-metot/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace string_metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Dersimiz CSharp, Hoşgeldiniz!";
+            string degisken2 = "Dersimiz CSharp";
+
+            //Length
+            Console.WriteLine(degisken.Length);
+
+            //ToUpper , ToLower
+            Console.WriteLine(degisken.ToUpper());
+            Console.WriteLine(degisken.ToLower());
+
+            //Concat
+            Console.WriteLine(String.Concat(degisken, " Merhaba!"));
+
+            //Compare, CompareTo
+
+            Console.WriteLine(degisken.CompareTo(degisken2)); // 0,1,-1 olabilir
+            Console.WriteLine(String.Compare(degisken, degisken2, true)); // true ignoreCase için.
+
+            //Contains
+            Console.WriteLine(degisken.Contains(degisken2));
+            Console.WriteLine(degisken.EndsWith("Hoşgeldiniz!"));
+            Console.WriteLine(degisken.StartsWith("Merhaba"));
+
+            //IndexOf
+            Console.WriteLine(degisken.IndexOf("CS"));
+
+            //Insert
+            Console.WriteLine(degisken.Insert(0, "Hi "));
+
+            //LastIndexOf
+            Console.WriteLine(degisken.LastIndexOf("i"));
+
+            //PadLeft, PadRight
+            Console.WriteLine(degisken + degisken2.PadLeft(30));
+            Console.WriteLine(degisken + degisken2.PadLeft(30, '*'));
+            Console.WriteLine(degisken.PadRight(50) + degisken2);
+            Console.WriteLine(degisken.PadRight(50, '-') + degisken2);
+
+            //Remove
+            Console.WriteLine(degisken.Remove(10)); // 10 başlangıç indeksi.
+            Console.WriteLine(degisken.Remove(5, 3));
+            Console.WriteLine(degisken.Remove(0, 1));
+
+            //Replace
+            Console.WriteLine(degisken.Replace("CSharp", "C#"));
+            Console.WriteLine(degisken.Replace(" ", "*"));
+
+            //Split
+            Console.WriteLine(degisken.Split(' ')[1]);
+
+            //Substring
+            Console.WriteLine(degisken.Substring(4));
+            Console.WriteLine(degisken.Substring(4,6)); //4.indekten başlayıp 6 karakter getirir.
+
+            Console.ReadKey();
+        }
+    }
+}
