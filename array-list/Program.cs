8d1929b6fdcdce59ee06c20a217eaa807ec6cf2c
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-11-30 00:04:53 +0300

    collections

---
+++ array-list/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace array_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ArrayList list = new ArrayList();
+            //list.Add("Ayşe");
+            //list.Add(21);
+            //list.Add(true);
+            //list.Add('A');
+
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //AddRange
+            Console.WriteLine("*****Add Range*****");
+            //string[] colors = { "red", "black", "white" };
+            List<int> numbers = new List<int>() { 1, 8, 3, 7, 9, 92, 5 };
+            //list.AddRange(colors);
+            list.AddRange(numbers);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort
+            Console.WriteLine("*****Sort*****");
+            //list.Sort(); //run-time da hata verir.
+
+            //Binary Search
+            Console.WriteLine("*****Binary Search*****");
+            Console.WriteLine(list.BinarySearch(9));
+
+            //Reverse
+            Console.WriteLine("*****Reverse*****");
+            list.Reverse();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Clear listeyi temizler.
+            list.Clear();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //systems.collections.generic
+            Dictionary<int, string> users = new Dictionary<int, string>();
+            users.Add(10,"Ayşe Yılmaz");
+            users.Add(12,"Ahmet Yılmaz");
+            users.Add(18,"Deniz Arda");
+            users.Add(20,"Özcan Coşar");
+
+            //Dizinin elemanlarına erişim.
+            Console.WriteLine("***** Elamanlara Erişim *****");
+            Console.WriteLine(users[12]);
+            foreach (var item in users)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Count
+            Console.WriteLine(users.Count);
+
+            //Contains
+            Console.WriteLine(users.ContainsKey(20));
+            Console.WriteLine(users.ContainsValue("Ali"));
+
+            //Remove
+            users.Remove(12);
+
+            //Keys
+            //Values
+            Console.ReadKey();
+        }
+    }
+}
