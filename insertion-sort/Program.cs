9ec822ee029534246aa40baef5e6ad8efd223494
Yasin Durgun <77541658+yasindurgun@users.noreply.github.com>
2021-12-27 23:58:20 +0300

    data structure hws

---
+++ insertion-sort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace insertion_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+int[] array = new int[6] { 22,27,16,2,18,6};
+
+
+
+ 1)
+
+
+ dizinin ilk elemanı diğer elemanlarla kıyaslanır en küçük olanla dizinin ilk elemanının yeri değiştirilir
+
+array = { 2,27,16,22,18,6};
+
+  2 elemanı zaten sıralama yapılarak geldiği için işleme tekrar sokulmaz 27 elemanlarla kıyaslanır ve 6 ile yeri değiştirilir
+
+array ={ 2,6,16,22,18, 27};
+
+  16 geri kalan elemanlarla kıyaslanır en küçük o olduğu için yer değişikliği olmaz
+
+array ={ 2,6,16,22,18, 27};
+
+ 22 diğerleriyle kıyaslanır
+
+array ={ 2,6,16,18,22, 27};
+
+ 22 tekrar kıyaslandığında zaten 27 den küçük olduğundan yer değişikliği olmaz
+
+array ={ 2,6,16,18,22, 27};
+
+
+
+
+ 2)  Big O notation nu ---> O(n^2) dir
+
+ average case : 
+ worst case :  n + (n-1) + (n-2) +....  --> 1den n e kadar olan sayı toplamı formülünden (n(n+1))/2 den en büyük katsayı alınacağından O(n^2) olur
+ best case : 
+
+  4) 18 dizinin ortasında olduğundan average case kapsamına girer
+
+             */
+        }
+    }
+}
